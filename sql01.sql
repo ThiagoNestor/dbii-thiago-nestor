9542ece8ffa96a7e3937df3a99e2d2016c6355ce
Thiago Nestor <thiagonestorafonso@gmail.com>
2025-04-01 00:43:45 -0300

    Terminei o código e o atualizei

--- sql01.sql
+++ sql01.sql
@@ -3,7 +3,8 @@ IDCliente int primary key auto_increment,
 Nome varchar (100) not null,
 Endereco varchar (225),
 Email varchar (100) unique,
-Senha varchar(225) not null
+Senha varchar(225) not null,
+DatAtt TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
 );
 
 
@@ -11,8 +12,9 @@ create table escolha(
 IDCliente int,
 IDObra int,
 primary key (IDCliente, IDObra),
-foreign key (IDCliente) references cliente(IDCliente),
-foreign key (IDObra) references Obra(IDObra)
+foreign key (IDCliente) references cliente(IDCliente) ON DELETE CASCADE,
+foreign key (IDObra) references Obra(IDObra) ON DELETE CASCADE,
+DatEscolha date not null
 );
 
 
@@ -21,7 +23,8 @@ IDObra int primary key auto_increment,
 Titulo varchar(225) not null,
 DatPubli date,
 Autor varchar(100),
-Tema varchar(100)
+Tema varchar(100),
+DatAtt TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
 );
 
 
@@ -30,9 +33,10 @@ IDCompAlg int primary key auto_increment,
 IDCliente int,
 DatCompAlg date not null,
 Preco decimal(10,2),
-TipoCompAlg enum ('Compra', 'Aluguel'),
-MetPag varchar(50),
-foreign key (IDcliente) references cliente(IDCliente)
+TipoCompAlg enum ('Compra', 'Aluguel') not null,
+MetPag varchar(50) not null,
+foreign key (IDcliente) references cliente(IDCliente) ON DELETE CASCADE,
+DatAtt TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
 );
 
 
@@ -40,8 +44,9 @@ create table e(
 IDCompAlg int,
 IDResg int not null,
 primary key (IDCompAlg, IDResg),
-foreign key (IDCompAlg) references compra_aluguel(IDCompAlg),
-foreign key (IDResg) references regietro(IDResg)
+foreign key (IDCompAlg) references compra_aluguel(IDCompAlg) ON DELETE CASCADE,
+foreign key (IDResg) references registro(IDResg) ON DELETE CASCADE,
+DatResg date not null
 );
 
 
@@ -52,7 +57,8 @@ DatResg date not null,
 DescResg text,
 Status enum('Ativo', 'Inativo'),
 Titulo varchar(225),
-foreign key (LocalArmaz) references armazenamento(LocalArmaz)
+foreign key (LocalArmaz) references armazenamento (LocalArmaz) ON DELETE SET,
+DatAtt TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
 );
 
 
@@ -60,16 +66,18 @@ create table local(
 IDResg int not null,
 LocalArmaz int,
 primary key (IDResg, LocalArmaz),
-foreign key (IDResg) references registro (IDResg),
-foreign key (LocalArmaz) references armazenamento (LocalArmaz)
+foreign key (IDResg) references registro (IDResg) ON DELETE CASCADE,
+foreign key (LocalArmaz) references armazenamento (LocalArmaz) ON DELETE CASCADE,
+DatAssociacao date not null
 );
 
 
-create table aramazenamento(
+create table armazenamento(
 LocalArmaz int primary key auto_increment,
 NomeArmaz varchar (100),
 NomeLocal varchar (100),
-ItemArmaz varchar (225)
+ItemArmaz varchar (225),
+DatAtt TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
 );
 
 
@@ -77,8 +85,8 @@ create table vai_para(
 IDResg int,
 IDHist int,
 primary key (IDResg, IDHist),
-foreign key (IDResg) references registro (IDResg),
-foreign key (IDHist) references historico (IDHist) 
+foreign key (IDResg) references registro (IDResg) ON DELETE CASCADE,
+foreign key (IDHist) references historico (IDHist) ON DELETE CASCADE
 );
 
 
@@ -87,7 +95,8 @@ IDHist int primary key auto_increment,
 TipoAlt varchar(100),
 DatAlt date not null,
 DescriAlt text,
-ItemAdd varchar (225)
+ItemAdd varchar (225),
+DatAtt TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
 );
 
 
@@ -95,5 +104,15 @@ create table faz_uma(
 IDHist int,
 IDAval int,
 primary key (IDHist, IDAval),
-foreign key ()
-)
+foreign key (IDHist) references historico (IDHist) ON DELETE CASCADE,
+foreign key (IDAval) references avaliacao (IDAval) ON DELETE CASCADE
+);
+
+create table avaliacao(
+IDAval int primary key auto_increment,
+Nota int check (nota between 1 and 5),
+Comentario text,
+DatAval date not null,
+Recomendacao boolean,
+DatAtt TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+);
